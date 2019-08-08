5e19b86200ce413479c7d65da550c0b8291da772
jaromirfranek <jaromir.franek@ibillboard.com>
2019-08-08 14:38:46 +0200

    testy test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
+
+interface Stuff {
+  id: string;
+  checked: boolean;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewChecked {
   title = 'speed-test';
+  currentTime: number;
+  stuffs: Array<Stuff> = [];
+
+  public ngOnInit(): void {
+    setInterval(() => {
+      this.currentTime = Date.now();
+      this.lazyGenerateStuff();
+    }, 5);
+  }
+
+  private lazyGenerateStuff() {
+    if (this.stuffs.length < 500) {
+      const newStuff = [];
+
+      for (let i = 0; i < 1; i++) {
+        newStuff.push({id: new Date().toISOString(), name: `Stuff-${i}`, checked: i % 2 === 0});
+      }
+      this.stuffs = [...this.stuffs, ...newStuff];
+    }
+  }
+
+  public ngAfterViewChecked(): void {
+  }
+
+  public trackByFn(index: number, data: Stuff) {
+    return data.id;
+  }
+
+  public createNewStuff(): void {
+    this.stuffs = [];
+
+    // this.lazyGenerateStuff();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {MatCheckboxModule, MatSlideToggleModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    MatCheckboxModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
