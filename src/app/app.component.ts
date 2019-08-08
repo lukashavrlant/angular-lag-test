1172adf359729f5028bd3cd3d3383f020e2166db
lukashavrlant <lukas.havrlant@ibillboard.com>
2019-08-08 16:33:55 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewChecked, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-interface Stuff {
+interface Data {
   id: string;
   checked: boolean;
   name: string;
@@ -9,42 +9,55 @@ interface Stuff {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, AfterViewChecked {
-  title = 'speed-test';
+export class AppComponent implements OnInit {
   currentTime: number;
-  stuffs: Array<Stuff> = [];
+  data: Array<Data> = [];
+  blockMsg = '';
 
   public ngOnInit(): void {
+    this.storeCurrentTime();
+    this.trackBlocks();
+  }
+
+  private trackBlocks() {
+    let lastCheckTime = Date.now();
+
+    setInterval(() => {
+      const now = Date.now();
+      const diff = now - lastCheckTime;
+
+      if (diff > 100) {
+        this.blockMsg = `The Event Loop was blocked for ${diff} ms`;
+      }
+
+      lastCheckTime = now;
+    }, 10);
+  }
+
+  private storeCurrentTime() {
     setInterval(() => {
       this.currentTime = Date.now();
-      this.lazyGenerateStuff();
-    }, 5);
+    }, 10);
   }
 
-  private lazyGenerateStuff() {
-    if (this.stuffs.length < 500) {
-      const newStuff = [];
+  private generateData() {
+    const newData = [];
 
-      for (let i = 0; i < 1; i++) {
-        newStuff.push({id: new Date().toISOString(), name: `Stuff-${i}`, checked: i % 2 === 0});
-      }
-      this.stuffs = [...this.stuffs, ...newStuff];
+    for (let i = 0; i < 200; i++) {
+      newData.push({id: `data-${i}`, name: `Stuff-${i}`, checked: i % 2 === 0});
     }
+
+    this.data = [...newData];
   }
 
-  public ngAfterViewChecked(): void {
-  }
-
-  public trackByFn(index: number, data: Stuff) {
+  public trackByFn(index: number, data: Data) {
     return data.id;
   }
 
   public createNewStuff(): void {
-    this.stuffs = [];
-
-    // this.lazyGenerateStuff();
+    this.generateData();
   }
 
 }
